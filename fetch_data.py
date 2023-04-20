5509bc1a691e6271e882bce2f8553693008cd415
krishmurarka <murarka.krish@gmail.com>
2023-04-20 20:33:05 +0530

    API for stocks data fetch

---
+++ fetch_data.py
@@ -0,0 +1,63 @@
+import os
+import io
+from dotenv import load_dotenv
+import pandas as pd
+import requests
+import json
+import ta
+from datetime import datetime, timedelta,date
+
+load_dotenv()
+BASE_URL = os.getenv("BASE_URL")
+def fetch_daily_prices(symbol,API_KEY):
+    params = {
+        'function': 'TIME_SERIES_DAILY_ADJUSTED',
+        'symbol': symbol,
+        'apikey': API_KEY,
+        'outputsize': 'compact',
+        'datatype': 'csv'
+    }
+    response = requests.get(BASE_URL, params=params)
+    data = pd.read_csv(io.StringIO(response.content.decode('utf-8')))
+    return data
+def calculate_technical_indicators(data):
+    # Convert to pandas DataFrame
+    df = pd.DataFrame(data)
+    df = df.sort_values('timestamp')
+    df['20ma'] = ta.trend.sma_indicator(df['close'], window=20)
+    df['50ma'] = ta.trend.sma_indicator(df['close'], window=50)
+    # Calculate relative strength index (RSI)
+    df['rsi'] = ta.momentum.RSIIndicator(df['close']).rsi()
+
+    # Calculate volatility
+    df['volatility'] = ta.volatility.AverageTrueRange(df['high'], df['low'], df['close']).average_true_range()
+
+    # Calculate volume data
+    df['volume'] = ta.volume.on_balance_volume(df['close'], df['volume'])
+    df = df.sort_values('timestamp', ascending=False)
+    return df
+
+def get_api_key(api_counter):
+    API_KEY = os.getenv("API_KEY_1")
+    if api_counter == 1:
+        API_KEY = os.getenv("API_KEY_2")
+    if api_counter == 2:
+        API_KEY = os.getenv("API_KEY_3")
+
+def fetch_data(symbol,api_counter):
+    symbol += ".BSE"
+    API_KEY = get_api_key(api_counter)
+    filename = f"{symbol}_data_{date.today()}.csv"
+    stock_data=""
+    if not os.path.exists(filename):
+        daily_prices = fetch_daily_prices(symbol)
+        # data = daily_prices
+        data = calculate_technical_indicators(daily_prices)
+        data.to_csv(filename, index=False)
+        stock_data = data
+    else: 
+        stock_data = pd.read_csv(filename)
+
+    print(stock_data)
+
+    return stock_data
---
+++ main.py
@@ -0,0 +1,23 @@
+
+from fastapi import FastAPI
+from fetch_data import fetch_data
+# from preprocess_data import preprocess_data
+# from predictor import predict
+
+app = FastAPI()
+api_counter = 0
+@app.get("/api/v1/stock_prices/{symbol}")
+def predict_stock_prices(symbol: str):
+    global api_counter
+    if api_counter == 3:
+        api_counter =0
+    data = fetch_data(symbol, api_counter)
+    api_counter+=1
+    # data = preprocess_data(data)
+    # forecast = predict(data)
+    json_data = data.to_json(orient="records")
+    return json_data
+
+
+
+# print(" the api key is ", api_key)
